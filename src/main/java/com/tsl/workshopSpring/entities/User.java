67ad0f077c60c6f39a98b6b255e32aaf8495c0d9
ThiagoSilvaLima <th.silvalima06@gmail.com>
2023-03-08 19:03:54 -0300

    h2 database, user profile, JPA

--- src/main/java/com/tsl/workshopSpring/entities/User.java
+++ src/main/java/com/tsl/workshopSpring/entities/User.java
@@ -2,10 +2,19 @@ package com.tsl.workshopSpring.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID=1L;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String name;
     String email;
